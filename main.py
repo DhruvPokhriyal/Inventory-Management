160c8d9d22b1dc975a70cb68a73639e8668182be
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-12-17 11:08:02 +0530

    Create msg and set it's text content

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from googleapiclient.discovery import build 
+from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from datetime import datetime
 import smtplib
+from email.message import EmailMessage
 
-MY_EMAIL = "";
-PASSWORD = "";
+MY_EMAIL = ""
+PASSWORD = ""
+with open("email_content.txt") as file:
+    msg = EmailMessage()
+    mail_template = file.read()
+    msg['Subject'] = "Item return"
+    msg.set_content(mail_template)
 
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
@@ -12,7 +18,9 @@ connection.login(user=MY_EMAIL, password=PASSWORD)
 
 acc_scopes = ["https://www.googleapis.com/auth/spreadsheets"]
 spreadsheetid = "1sRZsNiwImNwalonyiKfDyF0N9_UGduJBPYCDwbp-bos"
-range = "Sheet1!A4:H" # https://developers.google.com/sheets/api/guides/concepts
+#FIXME: Change variable name from range to anything else meaningful since it is shadowing built-in name range which
+# might cause problem in future
+range = "Sheet1!A4:H"  # https://developers.google.com/sheets/api/guides/concepts
 dateformat = "%d/%m/%Y"
 
 credentials = service_account.Credentials.from_service_account_file("creds.json", scopes=acc_scopes)
@@ -33,6 +41,4 @@ if not values:
 for row in values:
     isDue = datetime.strptime(row[7], dateformat) < datetime.now()
     print(row, "Duedate past:", isDue)
-
-
 connection.close()
